4e6f3f1c5c03ddd487021d542b3a0e10aa66da19
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-05 16:58:18 +0200

    fix: :fire: remove Welcome component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script>
 	import { base } from "$app/paths";
 	import Footer from "$lib/Footer.svelte";
-	import Welcome from "$lib/Welcome.svelte";
 </script>
 
 <main class="[&>*]:m-4">
-	<Welcome />
+  <h1 class="text-3xl">Book Ducks</h1>
 </main>
 
 <Footer />
