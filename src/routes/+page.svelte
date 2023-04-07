989b204a3fbc62679118dd37982de95fac393a25
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-07 15:44:37 +0200

    feat: :sparkles: use onMount to get the books when the page is loaded

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
 <script>
 	import { base } from "$app/paths";
+	import { onMount } from "svelte";
+	import { getBooks } from "$lib/api";
 	import Footer from "$lib/Footer.svelte";
+	import Book from "$lib/Book.svelte";
+
+	let books = [];
+
+	onMount(async () => {
+		books = await getBooks();
+		console.log(books);
+	});
 </script>
 
 <main class="[&>*]:m-4">
-  <h1 class="text-3xl">Book Ducks</h1>
+	<h1 class="text-3xl">Book Ducks</h1>
+
+	{#each Object.values(books) as book}
+		<Book {book} />
+	{/each}
 </main>
 
 <Footer />
