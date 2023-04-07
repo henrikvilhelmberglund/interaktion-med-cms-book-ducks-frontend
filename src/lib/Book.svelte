c6afbc72913dd37ecf2146414044076b78d2f683
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-07 16:33:37 +0200

    feat(components): :sparkles: add ability to close modal by clicking outside or pressing escape

--- src/lib/Book.svelte
+++ src/lib/Book.svelte
@@ -16,26 +16,66 @@
 	let synopsis = book.attributes.synopsis;
 
 	let expanded = false;
+
+	function clickOutside(element) {
+		function onEsc(event) {
+			if (event.key === "Escape") {
+				console.log("pressed esc - closing!");
+        expanded = false;
+			}
+		}
+
+		function onClick(event) {
+			if (!element.contains(event.target)) {
+				console.log("clicked outside of modal - closing!");
+				expanded = false;
+			} else {
+				console.log("clicked in modal");
+			}
+		}
+		document.body.addEventListener("click", onClick);
+		document.body.addEventListener("keydown", onEsc);
+		return {
+      update(newCallbackFunction) {
+        callbackFunction = newCallbackFunction;
+			},
+			destroy() {
+        document.body.removeEventListener("click", onClick);
+        document.body.removeEventListener("keydown", onEsc);
+			},
+		};
+	}
 </script>
 
 <article class="relative w-72">
-	<button on:click={() => (expanded = !expanded)}>
+	<button
+		on:click={() => {
+			expanded = true;
+			event.stopPropagation();
+		}}>
 		<img src="http://127.0.0.1:1337{cover_image}" alt={cover_image_alt} />
 	</button>
-	<div class="absolute left-[50%] top-[2%] w-[85%] translate-x-[-50%]">
+	<div class="absolute left-[50%] top-[2%] w-[95%] translate-x-[-50%]">
 		<h2 class="font-c text-center text-4xl font-bold text-black">{title}</h2>
 		<h3 class="translate-y-[10%] text-center text-xl">{author}</h3>
 	</div>
 	{#if expanded}
-		<div class="fixed inset-0 !m-0 bg-white" />
+		<div class="fixed inset-0 z-50 !m-0 backdrop-blur-lg" />
 
 		<div class="flex">
-			<p>{release_date}</p>
-			<p>{page_count}</p>
-			<p>{average_rating}</p>
-		</div>
-		<div>
-			{@html DOMPurify.sanitize(marked.parse(synopsis))}
+			<div use:clickOutside class="z-100 fixed left-0 top-0 bg-slate-200">
+				<img src="http://127.0.0.1:1337{cover_image}" alt={cover_image_alt} />
+        <div class="absolute left-[50%] top-[2%] w-[95%] translate-x-[-50%]">
+          <h2 class="font-c text-center text-7xl font-bold text-black">{title}</h2>
+          <h3 class="translate-y-[10%] text-center text-4xl">{author}</h3>
+        </div>
+        <p>{title} was released in {release_date}.</p>
+				<p>Page count: {page_count}</p>
+				<p>Average rating: {average_rating}</p>
+			</div>
+			<div>
+				{@html DOMPurify.sanitize(marked.parse(synopsis))}
+			</div>
 		</div>
 	{/if}
 </article>
