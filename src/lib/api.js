e11916f335fcdafc6bc571f21c4417bbeb47926d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-07 15:43:12 +0200

    feat(api): :sparkles: add NonAuth version of sendRequest and use it for now

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,10 @@
 import axios from "axios";
+
+export const getBooks = async () => {
+  const data = await sendRequestNonAuth("GET", "http://127.0.0.1:1337/api/books?populate=*");
+  return data;
+};
+
 export const sendRequest = async (method, url, data = null) => {
 	try {
 		const headers = {
@@ -8,6 +14,20 @@ export const sendRequest = async (method, url, data = null) => {
 			method,
 			url,
 			headers,
+			data,
+		});
+		return response.data.data;
+	} catch (e) {
+		console.error(e);
+		throw e.request;
+	}
+};
+
+export const sendRequestNonAuth = async (method, url, data = null) => {
+	try {
+		const response = await axios({
+			method,
+			url,
 			data,
 		});
 		return response.data.data;
