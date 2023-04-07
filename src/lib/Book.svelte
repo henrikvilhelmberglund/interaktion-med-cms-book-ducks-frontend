71e37b2804438faf28b4bc9e5dff477fa6aa19ba
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-07 15:44:00 +0200

    fix(components): :bug: take in a single prop instead and get the values from there

--- src/lib/Book.svelte
+++ src/lib/Book.svelte
@@ -3,20 +3,25 @@
 	import DOMPurify from "dompurify";
 	import { marked } from "marked";
 
-	export let title;
-	export let author;
-	export let page_count;
-	export let average_rating;
-	export let release_date;
-	export let cover_image;
-	export let cover_image_alt;
-	export let synopsis;
+	export let book;
+	console.log(book);
+	let title = book.attributes.title;
+	let author = book.attributes.author;
+	let page_count = book.attributes.page_count;
+	let average_rating = book.attributes.average_rating;
+	let release_date = book.attributes.release_date;
+	let cover_image = book.attributes.cover_image.data.attributes.url;
+	console.log(cover_image);
+	let cover_image_alt = book.attributes.cover_image;
+	let synopsis = book.attributes.synopsis;
 </script>
 
-<article>
-	<img src="{base}/{cover_image}" alt={cover_image_alt} />
-	<h2 class="text-3xl">{title}</h2>
-	<h3 class="text-xl">{author}</h3>
+<article class="relative border-red border-1 w-72">
+  <img src="http://127.0.0.1:1337{cover_image}" alt={cover_image_alt} />
+	<div class="absolute left-[50%] translate-x-[-50%] w-[80%] top-[2%]">
+    <h2 class="text-center text-3xl">{title}</h2>
+		<h3 class="translate-y-[10%] text-center text-xl">{author}</h3>
+	</div>
 	<div class="flex">
 		<p>{release_date}</p>
 		<p>{page_count}</p>
