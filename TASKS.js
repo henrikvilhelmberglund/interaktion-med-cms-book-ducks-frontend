d62398d20521cd4a8bb229781954a70061e97510
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-08 22:49:27 +0200

    fix: :memo: update TASKS.js

--- TASKS.js
+++ TASKS.js
@@ -4,9 +4,9 @@
 
 Du har fått i uppdrag att ta fram en applikation för bokaffären **Book Ducks**, där de kan ladda upp böcker som de säljer. Användare ska också kunna logga in, betygsätta samt spara böcker.
 
-!- Admin ska kunna ladda upp böcker via CMS:ets gränssnitt.
-!- Varje bok ska ha en titel, författare, antal sidor, snittbetyg (skala t.ex 1-10), utgivningsdatum samt en bild på bokomslaget. OBS! När en bok skapas upp, ska snittbetyg inte ha något värde än.
-!- Ladda upp minst 10 böcker i CMS:et.
+*- Admin ska kunna ladda upp böcker via CMS:ets gränssnitt.
+*- Varje bok ska ha en titel, författare, antal sidor, snittbetyg (skala t.ex 1-10), utgivningsdatum samt en bild på bokomslaget. OBS! När en bok skapas upp, ska snittbetyg inte ha något värde än.
+*- Ladda upp minst 10 böcker i CMS:et.
 
 Utloggat läge
 
@@ -17,9 +17,9 @@ Inloggat läge
 
 !- Det ska tydligt framgå vem som är inloggad på sidan. Användare ska kunna logga ut.
 !- Användaren ska kunna betygsätta böcker.
-!- Användare ska kunna spara böcker i en personlig Att läsa-lista.
+!- Användare ska kunna spara böcker i en personlig Att läsa-lista. Denna lista ska användare kunna se på en profilsida.
 !- (VG) - Admin ska med hjälp av admin-gränssnittet kunna välja mellan minst två olika färgteman på webbplatsen.
-!- (VG) - Det ska även finnas en profilsida där registrerade användare kan se sin Att läsa-lista samt en lista över samtliga böcker som den användaren betygsatt. Dessa ska kunna sorteras på titel, författare samt betyg (titel och författare sorteras i bokstavsordning A-Ö, betyg från högst till lägst).
+!- (VG) - Profilsidan ska även innehålla en lista över samtliga böcker som den användaren betygsatt. Dessa ska kunna sorteras på titel, författare samt betyg (titel och författare sorteras i bokstavsordning A-Ö, betyg från högst till lägst).
 !- Sidan ska ha ett (någorlunda) professionellt utseende.
 !- Sidan ska fungera utan några större buggar.
 
