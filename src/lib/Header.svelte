48b6c225c9b376e47fb92fffce861cb5db0e4c02
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-12 15:07:16 +0200

    feat(components): :lipstick: add a million divs to have the title in the center

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,13 +3,18 @@
 	export let myUser;
 </script>
 
-<header class="flex items-center justify-end bg-slate-800 p-6">
-	{#if myUser.hasOwnProperty("user")}
-		<p class="mr-4 text-white">{myUser.user.username}</p>
-	{:else}
-		<p class="mr-4 text-white">Not logged in</p>
-	{/if}
-	<LoginRegister bind:myUser />
+<header class="flex bg-slate-300 p-6 shadow-lg">
+	<div class="flex flex-1 justify-center">
+		<h1 class="text-5xl text-black">Book Ducks</h1>
+	</div>
+	<div class="flex items-center justify-end">
+		{#if myUser.hasOwnProperty("user")}
+			<p class="mr-4 text-black">{myUser.user.username}</p>
+		{:else}
+			<p class="mr-4 text-black">Not logged in</p>
+		{/if}
+		<LoginRegister bind:myUser />
+	</div>
 </header>
 
 <style>
