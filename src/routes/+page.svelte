02d111ed32dc1875ea7300eec94594f9a4d5f776
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-12 15:08:29 +0200

    fix: :fire: remove log out stuff and popups from main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,6 @@
 
 	let books = [];
 	let myUser = {};
-	let showLoginPopup = false;
-
-	$: {
-		if (myUser.hasOwnProperty("user")) {
-			showLoginPopup = true;
-			setTimeout(() => {
-				showLoginPopup = false;
-			}, 1500);
-		}
-	}
 
 	onMount(async () => {
 		books = await getBooks();
@@ -27,25 +17,7 @@
 </script>
 
 <Header bind:myUser/>
-<button
-	on:click={() => {
-		sessionStorage.removeItem("token");
-		location.reload();
-	}}
-	class="absolute right-4 rounded bg-blue-400 p-2 hover:bg-blue-300">Clear token</button>
-
 <main class="[&>*]:m-4">
-	{#if myUser.hasOwnProperty("user") && showLoginPopup}
-		<div
-			id="login-popup"
-			class="z-100 absolute left-[50%] top-0 mt-4 translate-x-[-50%] rounded-lg bg-green-500 p-4"
-			transition:fly={{ y: 20 }}>
-			<p>Successfully logged in as {myUser.user?.username}!</p>
-		</div>
-	{/if}
-	<h1 class="flex justify-center text-3xl">Book Ducks</h1>
-
-
 	<div class="flex flex-wrap justify-end gap-4 md:justify-start">
 		{#each Object.values(books) as book}
 			<Book {book} />
