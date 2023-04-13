35f129dc67f50dbacc5829e5662d956497047013
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-13 22:31:57 +0200

    feat(api): :sparkles: add updateUserRating
    createUserRating is TODO

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,52 @@
 import axios from "axios";
-import { token } from "./stores";
-import { get } from 'svelte/store'
+import { token, userRatingObject } from "./stores";
+import { get } from "svelte/store";
 
 export const getBooks = async () => {
 	const data = await sendRequestNonAuth("GET", "http://127.0.0.1:1337/api/books?populate=*");
 	return data;
 };
+
+// TODO
+export const createUserRating = async (id, newRating) => {
+	const average_rating = newRating;
+	console.log(average_rating);
+	const data = await sendRequest("PUT", `http://127.0.0.1:1337/api/books/${id}`, {
+		data: {
+			average_rating: average_rating,
+		},
+	});
+	return data;
+};
+
+export const updateUserRating = async (userRatingObject, book_id, newRating) => {
+	console.log(userRatingObject[book_id].rating_id);
+	const data = await sendRequest(
+		"PUT",
+		`http://127.0.0.1:1337/api/ratings/${userRatingObject[book_id].rating_id}`,
+		{
+			data: {
+				half_stars: newRating,
+			},
+		}
+	);
+	return data;
+};
+
+// no axios error??
+// export const updateUserRating = async (userRatingObject, book_id) => {
+// 	console.log(userRatingObject[book_id].rating_id);
+// 	const data = await sendRequest(
+// 		"PUT",
+// 		`http://127.0.0.1:1337/api/ratings/${userRatingObject[book_id].rating_id}`,
+// 		{
+// 			data: {
+// 				half_stars: userRatingObject.userRating,
+// 			},
+// 		}
+// 	);
+// 	return data;
+// };
 
 export const updateAverageRating = async (id, newRating) => {
 	const average_rating = newRating;
@@ -29,6 +70,7 @@ export const sendRequest = async (method, url, data = null) => {
 			headers,
 			data,
 		});
+		// console.log(response.data.data);
 		return response.data.data;
 	} catch (e) {
 		console.error(e);
