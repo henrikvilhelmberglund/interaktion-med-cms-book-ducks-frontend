ad87965b6f0f503eff023cc9b21b57ea8c01c66a
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-14 22:56:54 +0200

    feat: :sparkles: add mode 2 to enumCreator for creating safelist strings

--- enumCreator.js
+++ enumCreator.js
@@ -22,15 +22,25 @@ const types = [
 	"fill",
 	"stroke",
 ];
-const colors = ["primary", "secondary"];
+const colors = ["primary", "secondary", "base"];
 const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
+let mode = 2;
+let myArray = [];
 
 types.forEach((type) => {
 	console.log("[");
 	colors.forEach((color) => {
 		shades.forEach((shade) => {
-			console.log(`"${type}-${color}-${shade}",`);
+			if (mode === 1) {
+				console.log(`"${type}-${color}-${shade}",`);
+			} else if (mode === 2) {
+				myArray.push(`${type}-${color}-${shade}`);
+			}
 		});
 	});
 	console.log("],");
 });
+
+if (mode === 2) {
+	console.log(myArray.join(" "));
+}
