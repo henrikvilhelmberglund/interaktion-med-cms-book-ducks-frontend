b0d75fb33404192838aa55bf6c180230736119ef
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-14 22:19:39 +0200

    feat(themes): :sparkles: add themetesting route for testing theme stuff

---
+++ src/routes/themetesting/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { onMount } from "svelte";
+	let themes = ["blue", "yellow"];
+
+	function activateTheme(theme) {
+    document.documentElement.classList = "";
+		document.documentElement.classList.add(theme);
+	}
+	onMount(() => {
+		// activateTheme();
+	});
+</script>
+
+<main class="[&>*]:m-4">
+	<div class="bg-primary-700 h-32 w-32">hello</div>
+	<div class="blue bg-primary-500 h-32 w-32">hello</div>
+	<div class="blue bg-gray-600 h-32 w-32">hello</div>
+	{#each themes as theme}
+		<button on:click={() => activateTheme(theme)} class="btn">Toggle {theme}</button>
+	{/each}
+</main>
+
+<style>
+</style>
