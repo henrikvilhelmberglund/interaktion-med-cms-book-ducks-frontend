59c1d70dd74749d721e41868e04d4ea7152ebacf
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-14 14:25:53 +0200

    feat(api): :sparkles: add getCurrentUserAndRatings() and sendRequestMe() functions

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,10 +1,22 @@
 import axios from "axios";
-import { token, userRatingObject } from "./stores";
+import { token, userRatingObject, myUser } from "./stores";
 import { get } from "svelte/store";
 
 export const getBooks = async () => {
 	const data = await sendRequestNonAuth("GET", "http://127.0.0.1:1337/api/books?populate=*");
 	return data;
+};
+
+export const getCurrentUserAndRatings = async () => {
+	const data = await sendRequestMe(
+		"GET",
+		"http://127.0.0.1:1337/api/users/me?populate[ratings][populate][0]=books"
+	);
+
+	// console.log("data is " + data);
+	console.log(get(myUser));
+	myUser.set(data);
+	// console.log(get(myUser));
 };
 
 // TODO
@@ -59,6 +71,25 @@ export const updateAverageRating = async (id, newRating) => {
 	return data;
 };
 
+// to remember that Me is different for some reason
+export const sendRequestMe = async (method, url, data = null) => {
+	try {
+		const headers = {
+			Authorization: `Bearer ${get(token)}`,
+		};
+		const response = await axios({
+			method,
+			url,
+			headers,
+			data,
+		});
+		return response.data;
+	} catch (e) {
+		console.error(e);
+		throw e.request;
+	}
+};
+
 export const sendRequest = async (method, url, data = null) => {
 	try {
 		const headers = {
@@ -70,7 +101,6 @@ export const sendRequest = async (method, url, data = null) => {
 			headers,
 			data,
 		});
-		// console.log(response.data.data);
 		return response.data.data;
 	} catch (e) {
 		console.error(e);
