d7d03812564ffffed64abef4356767f834c6d978
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-15 21:03:46 +0200

    feat(themes): :lipstick: add some theme styling for Header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,20 +1,20 @@
 <script>
 	import LoginRegister from "$lib/LoginRegister.svelte";
-	import { bookExpanded, myUser } from "./stores";
+	import { activeTheme, bookExpanded, myUser } from "./stores";
   import { slide } from 'svelte/transition';
 
 </script>
 
 {#if !Object.keys($bookExpanded).length}
-	<header transition:slide class="z-100 sticky left-0 top-0 flex bg-slate-300 p-6 shadow-lg">
+	<header id="book-ducks-header" transition:slide class="z-100 sticky left-0 top-0 flex p-6 shadow-lg">
 		<div class="flex flex-1 justify-center">
-			<h1 class="text-5xl text-black">Book Ducks</h1>
+			<h1 class="text-5xl font-logo text-base-100">Book Ducks</h1>
 		</div>
 		<div class="flex items-center justify-end">
 			{#if $myUser.username}
-				<p class="mr-4 text-black">{$myUser.username}</p>
+				<p class="mr-4 text-base-300">{$myUser.username}</p>
 			{:else}
-				<p class="mr-4 text-black">Not logged in</p>
+				<p class="mr-4 text-base-300">Not logged in</p>
 			{/if}
 			<LoginRegister />
 		</div>
