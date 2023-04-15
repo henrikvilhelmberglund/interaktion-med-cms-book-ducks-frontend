c1e01d40530090d1485accbdc09e0bbc85a5acd8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-15 21:03:13 +0200

    feat(themes): :sparkles: add activeTheme store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -3,6 +3,7 @@ import { persisted } from "svelte-local-storage-store";
 
 export const myUser = writable({});
 export const bookExpanded = writable({});
+export const activeTheme = writable("");
 export const userRatingObject = writable({});
 export const token = persisted("token", null, {
 	storage: "session", // 'session' for sessionStorage, defaults to 'local'
