8b441962ed605085b90704e9bee724f813974cb2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-17 17:24:41 +0200

    feat: :sparkles: set theme stuff from layout instead of index page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script>
 	import Header from "$lib/Header.svelte";
 	import ThemeTester from "$lib/ThemeTester.svelte";
+	import { getTheme } from "$lib/api";
+	import { activateTheme } from "$lib/helpers";
+	import { activeTheme } from "$lib/stores";
 	import "@unocss/reset/tailwind.css";
+	import { onMount } from "svelte";
+
+	onMount(async () => {
+		let data = await getTheme();
+		$activeTheme = data.attributes.theme;
+		console.log($activeTheme);
+		$activeTheme = activateTheme($activeTheme);
+	});
 </script>
 
 <Header />
