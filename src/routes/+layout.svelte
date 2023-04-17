d377aca22b3395dc74528c29be422efd1b4dc075
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-17 15:01:27 +0200

    feat: :sparkles: add Header and ThemeTester to layout so they are visible in to-read-list route as well

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
+	import Header from "$lib/Header.svelte";
+	import ThemeTester from "$lib/ThemeTester.svelte";
 	import "@unocss/reset/tailwind.css";
 </script>
+
+<Header />
+
+<ThemeTester />
 
 <slot />
 
