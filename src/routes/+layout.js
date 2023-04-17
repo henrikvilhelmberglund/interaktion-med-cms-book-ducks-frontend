deb4e140dc210111d0bf07918f57d07af1a81ab6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-17 14:54:29 +0200

    feat: :sparkles: add +layout.js with load function that checks token and if there sets myUser store

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,22 @@
+import { getCurrentUserAndRatings } from "$lib/api";
+import { setUserRatingObject } from "$lib/helpers";
+import { myUser, token, userRatingObject } from "$lib/stores";
+import { get } from "svelte/store";
+
 export const prerender = true;
+userRatingObject;
+
+async function setMyUser() {
+	if (get(token)) {
+		let currentUser;
+		currentUser = await getCurrentUserAndRatings();
+		myUser.set(currentUser);
+		// $myUser = $myUser;
+		setUserRatingObject();
+	}
+}
+
+/** @type {import('./$types').PageLoad} */
+export async function load() {
+	await setMyUser();
+}
