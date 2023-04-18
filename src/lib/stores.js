19739c4f2c952003d19d185835c68c12d57d2d71
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-18 21:56:36 +0200

    feat(stores): :sparkles: add books store
    because I have no idea how to rerun load functions (maybe you don't)

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,6 +2,7 @@ import { writable } from "svelte/store";
 import { persisted } from "svelte-local-storage-store";
 
 export const myUser = writable({});
+export const books = writable({});
 export const bookExpanded = writable({});
 export const activeTheme = writable("");
 export const userRatingObject = writable({});
