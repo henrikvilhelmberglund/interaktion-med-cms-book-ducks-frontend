3debd78799c86dc6144db753828556e24e45d9cc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-18 22:05:40 +0200

    feat: :sparkles: add filtering buttons and logic
    note: if around each blocks were important for not getting errors

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,15 +1,12 @@
 <script>
-	import { browser } from "$app/environment";
 	import Book from "$lib/Book.svelte";
 	import LoginRegister from "$lib/LoginRegister.svelte";
 	import { sortList } from "$lib/helpers.js";
-	import { myUser, userRatingObject } from "$lib/stores";
-	import { afterUpdate } from "svelte";
+	import { books, myUser } from "$lib/stores";
+	import { beforeUpdate } from "svelte";
 	import { quintOut } from "svelte/easing";
 	import { crossfade } from "svelte/transition";
 	import { flip } from "svelte/animate";
-
-	export let data;
 
 	const [send, receive] = crossfade({
 		duration: (d) => Math.sqrt(d * 200),
@@ -31,33 +28,56 @@
 
 	let toReadBooks;
 	let filteredBooks;
-	let mode = "To read list";
-	let filterModes = ["Title Asc", "Title Desc", "Author Asc", "Author Desc"];
+	let ratedBooks;
+	// let mode = "To read list";
+	let mode = "Rated books list";
+	// let filterModes = ["Title Asc", "Title Desc", "Author Asc", "Author Desc"];
+	let filterModes = [
+		"Title Asc",
+		"Title Desc",
+		"Author Asc",
+		"Author Desc",
+		"Rating Asc",
+		"Rating Desc",
+	];
 	let activeFilterMode = "normal";
 
-	// ? this seems pretty stupid
-	afterUpdate(async () => {
+	// beforeUpdate(async () => {
+	// });
+
+	// * beforeUpdate is nice!
+	beforeUpdate(async () => {
 		try {
 			toReadBooks = $myUser.to_read_list.books;
+			ratedBooks = $myUser.ratings;
+			ratedBooks = ratedBooks.map((ratings) => {
+				ratings = ratings.books[0];
+				// console.log(ratings);
+				ratings = { id: ratings.id, attributes: { ...ratings } };
+				// console.log(ratings);
+				return ratings;
+			});
+			updateList();
 		} catch (error) {
 			// ! this is SSR and triggered an error because $myUser is not defined yet
 			return;
 		}
-		updateList();
 	});
 
 	function updateList() {
+		console.log("ran updateList");
 		if (mode === "To read list") {
 			let toReadBooksIDs = toReadBooks.map((book) => book.id);
-			filteredBooks = data.books.filter((book) => {
+			filteredBooks = Object.values($books).filter((book) => {
 				return toReadBooksIDs.includes(book.id);
 			});
 			// console.log(filteredBooks);
 			// console.log(filteredBooks);
 		} else if (mode === "Rated books list") {
-			let ratedBooksIDs = $myUser.ratings.map((book) => book.books[0].id);
+			let ratedBooksIDs = ratedBooks.map((book) => book.id);
 			// console.log(ratedBooksIDs);
-			filteredBooks = data.books.filter((book) => {
+
+			filteredBooks = Object.values($books).filter((book) => {
 				return ratedBooksIDs.includes(book.id);
 			});
 		}
@@ -66,9 +86,9 @@
 </script>
 
 <main class="bg-primary-100 dark:bg-base-900 pb-64 pt-4 [&>*]:m-4 [&>*]:mt-0">
-	<h2 class="text-4xl">Welcome, {$myUser.username}!</h2>
+	<h2 class="text-base-100 text-4xl">Welcome, {$myUser.username}!</h2>
 	<button
-		on:click={() => {
+		on:click={async () => {
 			if (mode === "To read list") {
 				mode = "Rated books list";
 				filterModes = [
@@ -83,6 +103,7 @@
 				mode = "To read list";
 				filterModes = ["Title Asc", "Title Desc", "Author Asc", "Author Desc"];
 			}
+			updateList();
 		}}
 		class="btn-secondary"
 		>Switch to {mode === "To read list" ? "Rated books list" : "To read list"}</button>
@@ -94,6 +115,7 @@
 				activeFilterMode = "normal";
 				// updateList();
 			}}
+			class:outline-solid={activeFilterMode === "normal"}
 			class="btn-primary">Clear sort</button>
 		{#each filterModes as filterMode}
 			<button
@@ -101,29 +123,37 @@
 					activeFilterMode = filterMode;
 					// updateList();
 				}}
+				class:outline-solid={activeFilterMode === filterMode}
 				class="btn-primary">{filterMode}</button>
 		{/each}
 		{#if mode === "To read list"}
 			<div class="flex flex-wrap">
-				{#each filteredBooks as book, i (book.id)}
-					<div
-						in:receive={{ key: book.id }}
-						out:send={{ key: book.id }}
-						animate:flip={{ duration: 1000 }}>
-						<Book {book} />
-					</div>
-				{/each}
+				{#if filteredBooks}
+					{#each filteredBooks as book, i (book.id)}
+						<div
+							in:receive={{ key: book.id }}
+							out:send={{ key: book.id }}
+							animate:flip={{ duration: 1000 }}>
+							<Book {book} />
+						</div>
+					{/each}
+				{/if}
 			</div>
 		{:else if mode === "Rated books list"}
 			<div class="flex flex-wrap">
-				{#each filteredBooks as book, i (book.id)}
-					<div
-						in:receive={{ key: book.id }}
-						out:send={{ key: book.id }}
-						animate:flip={{ duration: 1000 }}>
-						<Book {book} />
-					</div>
-				{/each}
+				{#if filteredBooks}
+					{#each filteredBooks as book, i (book.id)}
+						<div
+							in:receive={{ key: book.id }}
+							out:send={{ key: book.id }}
+							animate:flip={{ duration: 1000 }}>
+							<Book
+								{book}
+								isProfilePage={activeFilterMode === "Rating Desc" ||
+									activeFilterMode === "Rating Asc"} />
+						</div>
+					{/each}
+				{/if}
 			</div>
 		{/if}
 	{:else}
