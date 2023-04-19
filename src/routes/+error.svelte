bf73863679a52e8c67e0d8c151987e98aeaf8ace
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-19 10:28:47 +0200

    feat: :sparkles: add +error.svelte for other errors

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from "$app/stores";
+</script>
+
+<div class="flex items-center justify-center">
+  <img src="https://http.cat/{$page.status}" alt="" />
+</div>
+
+<style>
+</style>
