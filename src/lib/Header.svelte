f677e211d756a4c0c6466544cbd46dc40a1be86b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-19 14:26:49 +0200

    fix(components): :lipstick: improve styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -10,16 +10,16 @@
 	<header
 		id="book-ducks-header"
 		transition:slide
-		class="z-100 sticky left-0 top-0 flex p-2 md:p-6 shadow-lg">
+		class="z-100 sticky left-0 top-0 flex p-2 shadow-lg md:p-6">
 		<div class="flex flex-1 items-center" />
 		<div class="flex flex-1 items-center justify-center">
 			<h1 class="font-logo text-base-100 text-5xl">
 				<a href="{base}/">Book Ducks</a>
 			</h1>
 		</div>
-		<div class="flex flex-1 items-center justify-end">
+		<div class="flex md:flex-row flex-col flex-1 items-center justify-end">
 			{#if !$myUser.username}
-				<p class="text-base-300 mr-4">Not logged in</p>
+				<p class="text-base-300 md:mr-4">Not logged in</p>
 			{/if}
 			<LoginRegister />
 			<button
@@ -27,6 +27,7 @@
 					$preferredMode = $preferredMode === "light" ? "dark" : "light";
 					activateTheme();
 				}}
+				aria-label="dark mode toggle"
 				class="i-carbon-sun !bg-base-100 dark:i-carbon-moon ml-4 text-3xl" />
 		</div>
 	</header>
