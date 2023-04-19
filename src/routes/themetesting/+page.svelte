bd6aa1ab5bbf094d0b1a5c747b211b3356086557
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-19 16:46:30 +0200

    fix: :bug: fix themetesting route

--- src/routes/themetesting/+page.svelte
+++ src/routes/themetesting/+page.svelte
@@ -1,9 +1,6 @@
 <script>
-	import Header from "$lib/Header.svelte";
 	import ThemeTester from "$lib/ThemeTester.svelte";
 </script>
-
-<Header />
 
 <ThemeTester />
 <div class="gif-BG h-full w-full" />
